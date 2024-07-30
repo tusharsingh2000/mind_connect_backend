0e0bf02d68aeec4bc14c26c3bf3d13b6ae53f8ce
Tushar <tusharsinghynr@gmail.com>
2024-07-30 23:23:21 +0530

    user model address add

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserModel = new Schema({
-
+const UserModel = new Schema(
+  {
     isSocialLogin: { type: Boolean, default: false },
     socialId: { type: String },
     socialType: { type: String, enum: ["FACEBOOK", "GOOGLE", "APPLE"] },
     role: { type: String, enum: ["user", "consultant"], default: "user" },
     fullName: { type: String, default: "", trim: true },
+    address: { type: String, default: "", trim: true },
     userName: { type: String, default: "", trim: true },
     firstName: { type: String, default: "", trim: true },
     lastName: { type: String, default: "", trim: true },
@@ -21,19 +22,19 @@ const UserModel = new Schema({
     isVerifiedByAdmin: { type: Boolean, default: false },
     password: { type: String, default: "", select: false },
     image: { type: String, default: "" },
-    coverImage: { type: String, default: "" }, 
+    coverImage: { type: String, default: "" },
     gender: { type: String, enum: ["MALE", "FEMALE", "OTHERS"] },
     jti: { type: String, default: "", trim: true },
     zipCode: { type: String, default: null },
     dob: { type: String, default: null },
     profileCompleteAt: { type: Number, default: 0 },
     location: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            default: "Point"
-        },
-        coordinates: { type: [Number], default: [0, 0] }
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: { type: [Number], default: [0, 0] },
     },
 
     deviceType: { type: String, default: "" },
@@ -45,32 +46,30 @@ const UserModel = new Schema({
     isBlocked: { type: Boolean, default: false, index: true },
     latitude: { type: String, default: 0 },
     longitude: { type: String, default: 0 },
-    types : [{ type: Schema.Types.ObjectId, ref: 'category' }], // user side used
-    isVerified: { type: Boolean, default: false },  // sp account verified from admin
-    categoryId: [{ type: Schema.Types.ObjectId, ref: 'category' }] // sp category
-
-
-
-}, {
+    types: [{ type: Schema.Types.ObjectId, ref: "category" }], // user side used
+    isVerified: { type: Boolean, default: false }, // sp account verified from admin
+    categoryId: [{ type: Schema.Types.ObjectId, ref: "category" }], // sp category
+  },
+  {
     timestamps: true,
     toObject: { virtuals: true },
-    toJSON: { virtuals: true }
-}
+    toJSON: { virtuals: true },
+  }
 );
 UserModel.index({ userLocation: "2dsphere" });
 
 UserModel.pre(/save|create|update/i, function (next) {
-    if (this.get("latitude") && this.get("longitude")) {
-        const longitude = this.get("longitude");
-        const latitude = this.get("latitude");
-        const geoPoint = {
-            type: "Point",
-            coordinates: [parseFloat(longitude), parseFloat(latitude)]
-        };
-        this.set({ geoPoint });
-    }
+  if (this.get("latitude") && this.get("longitude")) {
+    const longitude = this.get("longitude");
+    const latitude = this.get("latitude");
+    const geoPoint = {
+      type: "Point",
+      coordinates: [parseFloat(longitude), parseFloat(latitude)],
+    };
+    this.set({ geoPoint });
+  }
 
-    next();
+  next();
 });
 const User = mongoose.model("user", UserModel);
 module.exports = User;
