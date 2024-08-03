f30d5935a4ce73bb4d00b20ecdac9a1a8839aab5
Tushar <tusharsinghynr@gmail.com>
2024-08-03 19:12:37 +0530

    slot fix

--- models/slotsTime.js
+++ models/slotsTime.js
@@ -2,30 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const constant = require(".././utility/constant");
 
-let slotSchema = new Schema({
-
-    userId: { type: Schema.Types.ObjectId, ref: 'user', index: true },
+let slotSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "user", index: true },
     day: {
-        type: String,
-        enum: [constant.DAYS],
-        required : true
+      type: [String],
+      required: true,
     },
     openTime: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     closeTime: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     breakTime: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
-    isDeleted: { type: Boolean, default: false, index: true }
-
-}, {
-    timestamps: true
-});
+    isDeleted: { type: Boolean, default: false, index: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 let driverSlot = mongoose.model("slotsTime", slotSchema);
 module.exports = driverSlot;
